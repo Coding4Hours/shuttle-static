a97d607e5d126d5e60227dd802d7120659801855
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-02-19 13:32:12 -0800

    Update register-sw.js

--- register-sw.js
+++ register-sw.js
@@ -1,30 +1,32 @@
 "use strict";
 /**
- * Distributed with Ultraviolet and compatible with most configurations.
+ *
+ * @param {string} input
+ * @param {string} template Template for a search query.
+ * @returns {string} Fully qualified URL
  */
-const stockSW = "/uv-sw.js";
+function search(input, template) {
+  try {
+    // input is a valid URL:
+    // eg: https://example.com, https://example.com/test?q=param
+    return new URL(input).toString();
+  } catch (err) {
+    // input was not a valid URL
+  }
 
-/**
- * List of hostnames that are allowed to run serviceworkers on http:
- */
-const swAllowedHostnames = ["localhost", "127.0.0.1"];
+  try {
+    // input is a valid URL when http:// is added to the start:
+    // eg: example.com, https://example.com/test?q=param
+    const url = new URL(`http://${input}`);
+    // only if the hostname has a TLD/subdomain
+    if (url.hostname.includes(".")) return url.toString();
+  } catch (err) {
+    // input was not valid URL
+  }
 
-/**
- * Global util
- * Used in 404.html and index.html
- */
-async function registerSW() {
-  if (
-    location.protocol !== "https:" &&
-    !swAllowedHostnames.includes(location.hostname)
-  )
-    throw new Error("Service workers cannot be registered without https.");
+  // input may have been a valid URL, however the hostname was invalid
 
-  if (!navigator.serviceWorker)
-    throw new Error("Your browser doesn't support service workers.");
-
-  // Ultraviolet has a stock `sw.js` script.
-  await navigator.serviceWorker.register(stockSW, {
-    scope: __uv$config.prefix,
-  });
+  // Attempts to convert the input to a fully qualified URL have failed
+  // Treat the input as a search query
+  return template.replace("%s", encodeURIComponent(input));
 }
