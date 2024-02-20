36d184152faf75dd8cd3065b9ba21ebf7233f3ae
Coding4Hours <138321129+Coding4Hours@users.noreply.github.com>
2024-02-19 17:19:28 -0800

    Update uv.config.js

--- uv/uv.config.js
+++ uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/shuttle-uv/',
-    bare:'https://bare.benrogo.net',
+    bare:'https://usemetallic.com/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
